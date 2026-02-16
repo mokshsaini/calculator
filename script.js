7d4c7563c65f6460bac4c28a9cf8309cc636756a
Moksh Saini <125344860+mokshsaini@users.noreply.github.com>
2026-02-16 15:59:23 +0530

    decimal added

--- script.js
+++ script.js
@@ -1,29 +1,59 @@
-// Events for numbers and operators
-let screen = document.querySelector('#calScreen')
-let nodelist = document.querySelectorAll('.number-button, .operator-button')
+// Events for numbers, decimal and operators
+let screen = document.querySelector('#calScreen');
+let nodelist = document.querySelectorAll('.number-button, .operator-button, .decimal');
 nodelist.forEach((key)=>
     key.addEventListener('click', (e)=>{
-        // let screen = document.getElementById("display").querySelector('#calScreen')
-        let keyValue = e.target.value
+        let keyValue = e.target.value;
         if(screen.value === '0'){
-            screen.value = keyValue
+            screen.value = keyValue;
         }
         else{
-            screen.value += keyValue
+            screen.value += keyValue;
         }
     }, false)
 )
 
-// Event for clear and =
+// Event for clear, = and del
 document.querySelector('.reset').addEventListener('click', (e)=>{
-    document.querySelector('#calScreen').value = 0
-})
+    screen.value = 0;
+}, false);
+
 document.querySelector('.return-button').addEventListener('click', (e)=>{
     try{
-        screen.value = eval(screen.value)
+        screen.value = eval(screen.value);
     }
     catch(error){
-        screen.value = error
+        screen.value = SyntaxError();
     }
     
-})
+}, false);
+
+document.querySelector('.del').addEventListener('click', (e)=>{
+    screen.value = (screen.value.length === 1)? '0' : screen.value.slice(0, -1);
+}, false);
+
+
+// Keydown Events
+document.body.addEventListener('keydown', (e)=>{
+    let keyValue = e.key;
+    if(/[\d+/*-]/.test(keyValue)){      // we didn't use switch case because it compares with strict equality(===)
+        if(screen.value === '0'){
+            screen.value = keyValue;
+        }
+        else{
+            screen.value += keyValue;
+        }
+    }
+    else if(/=|Enter/.test(keyValue)){
+        try{
+            screen.value = eval(screen.value);
+        }
+        catch(error){
+            screen.value = SyntaxError();
+        }
+    }
+    else if(keyValue == "Backspace"){
+        screen.value = (screen.value.length === 1)? '0' : screen.value.slice(0, -1);
+    }
+
+}, false)
